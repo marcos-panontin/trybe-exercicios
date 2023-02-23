98ae2620c5c18e5be3f6f4c4b7d4b670942e1e13
Marcos Panontin <amarcospanontin@gmail.com>
2023-02-23 12:54:05 -0300

    Começando ultimo exercicio

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -74,37 +74,86 @@
 // }
 
 
-let pecaDeXadrez = 'arroz';
+// let pecaDeXadrez = 'arroz';
 
-switch (pecaDeXadrez.toLowerCase()) {
-    case "cavalo": console.log("L");
-        break;
-    case "bispo": console.log("Diagonals");
-        break;
-    case "peao": console.log("Um pra frente, dois se a primeira vez");
-        break;
-    case 'torre': console.log("HORINZONTALS");
-        break;
-    default: console.log("PECA INVALIDA");
+// switch (pecaDeXadrez.toLowerCase()) {
+//     case "cavalo": console.log("L");
+//         break;
+//     case "bispo": console.log("Diagonals");
+//         break;
+//     case "peao": console.log("Um pra frente, dois se a primeira vez");
+//         break;
+//     case 'torre': console.log("HORINZONTALS");
+//         break;
+//     default: console.log("PECA INVALIDA");
+// }
+
+// let notaPorcentagem = 74;
+// let notaConceito;
+
+// if (notaPorcentagem >= 90 && notaPorcentagem < 100) {
+//     notaConceito = "A";
+// } else if (notaPorcentagem >= 80 && notaPorcentagem < 90) {
+//     notaConceito = "B";
+// } else if (notaPorcentagem >= 70 && notaPorcentagem < 80) {
+//     notaConceito = "C";
+// } else if (notaPorcentagem >= 60 && notaPorcentagem < 70) {
+//     notaConceito = "BD";
+// } else if (notaPorcentagem >= 50 && notaPorcentagem < 60) {
+//     notaConceito = "E";
+// } else if (notaPorcentagem >= 0 && notaPorcentagem < 50) {
+//     notaConceito = "F";
+// } else {
+//     console.log("NOTA INVÁLIDA")
+// }
+
+// console.log(notaConceito)
+
+
+// let num1 = 1;
+// let num2 = 5;
+// let num3 = 3;
+
+// if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// let num1 = 1;
+// let num2 = 5;
+// let num3 = 3;
+
+// if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// let valorCusto = 0;
+// let valorVenda = 410;
+// let impostoSobreOCusto = valorCusto * 0.2;
+// let valorCustoTotal = valorCusto + impostoSobreOCusto;
+
+// let lucro;
+
+// if (valorCusto <= 0 || impostoSobreOCusto <= 0 || valorCustoTotal <= 0) {
+//     console.log("VALORES INVÁLIDOS")
+    
+// } else {
+//     lucro = valorVenda - valorCustoTotal;
+//     console.log(lucro)
+// }
+
+// cálculo salário líquido
+
+
+let salarioBruto;
+let aliquotaINSS;
+let salarioBase;
+let aliquotaIR;
+let salarioLiquido;
+
+if (salarioBruto <= 1556.94) {
+    
 }
-
-let notaPorcentagem = 74;
-let notaConceito;
-
-if (notaPorcentagem >= 90 && notaPorcentagem < 100) {
-    notaConceito = "A";
-} else if (notaPorcentagem >= 80 && notaPorcentagem < 90) {
-    notaConceito = "B";
-} else if (notaPorcentagem >= 70 && notaPorcentagem < 80) {
-    notaConceito = "C";
-} else if (notaPorcentagem >= 60 && notaPorcentagem < 70) {
-    notaConceito = "BD";
-} else if (notaPorcentagem >= 50 && notaPorcentagem < 60) {
-    notaConceito = "E";
-} else if (notaPorcentagem >= 0 && notaPorcentagem < 50) {
-    notaConceito = "F";
-} else {
-    console.log("NOTA INVÁLIDA")
-}
-
-console.log(notaConceito)
