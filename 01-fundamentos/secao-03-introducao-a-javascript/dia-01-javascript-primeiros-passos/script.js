f12bac3008a41e269a3890092610cdb9a2dbd78c
Marcos Panontin <amarcospanontin@gmail.com>
2023-02-26 13:44:07 -0300

    Exercicios pos aula2

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -45,7 +45,7 @@
 //     } else {
 //         console.log("MAIOR Número" + c);
 //     }
-// } 
+// }
 
 // let num = -9
 
@@ -139,7 +139,7 @@
 
 // if (valorCusto <= 0 || impostoSobreOCusto <= 0 || valorCustoTotal <= 0) {
 //     console.log("VALORES INVÁLIDOS")
-    
+
 // } else {
 //     lucro = valorVenda - valorCustoTotal;
 //     console.log(lucro)
@@ -148,12 +148,51 @@
 // cálculo salário líquido
 
 
-let salarioBruto;
-let aliquotaINSS;
-let salarioBase;
-let aliquotaIR;
-let salarioLiquido;
+// let salarioBruto = 3000;
+// let aliquotaINSS;
+// let salarioBase;
+// let aliquotaIR;
+// let salarioLiquido;
 
-if (salarioBruto <= 1556.94) {
-    
+// if (salarioBruto <= 1556.94) {
+//     aliquotaINSS = 0.08 * salarioBruto;
+// } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+//     aliquotaINSS = 0.09 * salarioBruto;
+// } else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+//     aliquotaINSS = 0.11 * salarioBruto;
+// } else if (salarioBruto > 5189.82) {
+//     aliquotaINSS = 570.88;
+// }
+
+// salarioBase = salarioBruto - aliquotaINSS
+
+
+// if (salarioBase <= 1903.98) {
+//     aliquotaIR = 0;
+// } else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+//     aliquotaIR = 0.075 * salarioBase - 142.8;
+// } else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+//     aliquotaIR = 0.15 * salarioBase - 354.8;
+// } else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+//     aliquotaIR = 0.225 * salarioBase - 636.13;
+// } else if (salarioBase >= 4664.68) {
+//     aliquotaIR = 0.275 * salarioBase - 869.36;
+// }
+
+// salarioLiquido = salarioBase - aliquotaIR
+// console.log(salarioBase);
+// console.log(aliquotaINSS);
+// console.log(aliquotaIR);
+// console.log(salarioLiquido);
+
+
+let pizzas = ["Palmito", "Mussarela", "Frango", "Calabresa"];
+
+// console.log(pizzas[0]);
+// console.log(pizzas[1]);
+// console.log(pizzas[2]);
+// console.log(pizzas[3]);
+
+for (let i = 0; i < 4; i = i + 1) {
+    console.log(pizzas[i])
 }
