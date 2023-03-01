a9e0b59c635d4b3d56d5c178facf573856ddc128
Marcos Panontin <amarcospanontin@gmail.com>
2023-03-01 13:06:46 -0300

    Renomear pasta dia 3

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/script.js
@@ -0,0 +1,68 @@
+// Considere a variável saldo, que representa a quantia em conta de determinada(o) cliente do TrybeBank e escreva quatro programas que:
+
+// Adicione um valor ao saldo.
+// Subtraia um valor do saldo.
+// Multiplique o valor do saldo por uma taxa.
+// Divida o valor do saldo.
+
+let balance = 100;
+
+function addValue(value) {
+    return (balance += value);
+}
+
+function subtractValue(value) {
+    return (balance -= value);
+}
+
+function multiplyValue(value) {
+    return (balance *= value);
+}
+
+function divideValue(value) {
+    return (balance /= value);
+}
+
+console.log(addValue(10));
+console.log(subtractValue(25));
+console.log(multiplyValue(10));
+console.log(divideValue(10));
+
+// Faça um programa para adicionar clientes ao array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro caso o parâmetro não seja do tipo string.
+
+let clientesTrybeBank = ["Ada", "John", "Gus"];
+
+function addClients(clientName) {
+    if (typeof clientName === "string") {
+        clientesTrybeBank.push(clientName);
+    } else {
+        console.log("Error. You need to enter a string!");
+    }
+}
+
+addClients("Clark");
+console.log(clientesTrybeBank);
+
+//Agora iremos escrever um programa que irá excluir um(a) cliente já existente do array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro, caso o parâmetro não seja do tipo string e caso o(a) cliente não exista dentro do array.
+
+
+function removeClients(clientName) {
+    if (typeof clientName === "string") {
+        if (clientesTrybeBank.includes(clientName)) {
+            let clientIndex = clientesTrybeBank.indexOf(clientName);
+            clientesTrybeBank.splice(clientIndex, 1);
+            return 'O cliente ' + clientName + " foi excluído com sucesso do sistema.";
+        } else {
+            return "Cliente não está no banco de dados!";
+        }
+    }
+    else {
+        return "Insira um nome válido.";
+    }
+}
+
+console.log(removeClients("Clark"));
+
+console.log(removeClients("Jack"));
+
+console.log(removeClients(4));
