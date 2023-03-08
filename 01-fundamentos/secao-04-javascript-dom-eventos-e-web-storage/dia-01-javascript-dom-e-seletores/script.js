e7b05e8b3a3fd1ce3374818ee47f8de365f7b378
Marcos Panontin <amarcospanontin@gmail.com>
2023-03-08 13:35:40 -0300

    Exercises pre-class

---
+++ 01-fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -0,0 +1,15 @@
+document.querySelector(".emergency-tasks").style.backgroundColor = "tomato";
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor =
+    "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor =
+    "purple";
+
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor =
+    "purple";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor =
+    "purple";
+
+    document.getElementById("header-container").style.backgroundColor = 'green'
