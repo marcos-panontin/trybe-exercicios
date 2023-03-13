f93575a531afa3922208069b6b258d2bc2f0cca8
Marcos Panontin <amarcospanontin@gmail.com>
2023-03-13 20:26:17 -0300

    Exercise done

---
+++ 01-fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll('button')
+const main = document.querySelector('main');
+
+const changeProperty = (event) => {
+    let property = event.target.parentNode.id;
+    let value = event.target.innerHTML;
+    main.style[property] = value;
+    localStorage.setItem(property, value);
+};
+
+for (let button of buttons) {
+    button.addEventListener("click", changeProperty);
+}
+
+for (let item of Object.entries(localStorage)) {
+    main.style[item[0]] = item[1];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
