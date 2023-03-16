26bf295c2b6ba91f063eccf2ca83ff9d3298d280
Marcos Panontin <amarcospanontin@gmail.com>
2023-03-16 18:49:20 -0300

    das

---
+++ 01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -0,0 +1,13 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+
+HREF_LINK.addEventListener('click', (e) => {e.preventDefault()})
+INPUT_CHECKBOX.addEventListener('click', (e) => {e.preventDefault()})
+INPUT_TEXT.addEventListener('keypress', (e) => {
+    if (e.key !== "a" && e.key !== "A") {
+        e.preventDefault();
+    };  
+})
